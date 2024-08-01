c89ec749a76e037f0be3d8d3449c52aa0b3def62
Orkun Doğan <od.orkun.dogan@gmail.com>
2024-08-01 21:58:03 +0300

    map controller fixes, app properties fixes
    Signed-off-by: Orkun Doğan <od.orkun.dogan@gmail.com>

--- src/main/java/org/codexist/restcontrollers/MapController.java
+++ src/main/java/org/codexist/restcontrollers/MapController.java
@@ -13,16 +13,6 @@ public class MapController {
 
     MapService mapService = new MapService();
 
-    @GetMapping("/hello")
-    public String helloWorld() {
-        return "Hello World";
-    }
-
-    @GetMapping("/hi")
-    public String hi() {
-        return "hi";
-    }
-
     @GetMapping("/findNearbyLocations/")
     public ArrayList<MapPoint> findNearbyLocations(@RequestParam double latitude, @RequestParam double longitude, @RequestParam double radius) {
         MapPoint requestMapPoint = new MapPoint(latitude, longitude);
